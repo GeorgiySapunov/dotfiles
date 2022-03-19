7b39b3594126e6421a73f54c8733466f2a6b4d32
Georgiy_Sapunov <georgiysapunov@yandex.ru>
2022-03-19 15:48:20 +0300

    rss

--- import.sh
+++ import.sh
@@ -8,6 +8,7 @@ rsync -uvrP --delete-after ~/.config/lf                 ~/git/dotfiles/.config
 rsync -uvrP --delete-after ~/.config/mpd/mpd.conf       ~/git/dotfiles/.config/mpd/
 rsync -uvrP --delete-after ~/.config/mpv                ~/git/dotfiles/.config
 rsync -uvrP --delete-after ~/.config/ncmpcpp            ~/git/dotfiles/.config
+rsync -uvrP --delete-after ~/.config/newsboat           ~/git/dotfiles/.config
 rsync -uvrP --delete-after ~/.config/nvim               ~/git/dotfiles/.config
 rsync -uvrP --delete-after ~/.config/pipewire           ~/git/dotfiles/.config
 rsync -uvrP --delete-after ~/.config/shell              ~/git/dotfiles/.config
