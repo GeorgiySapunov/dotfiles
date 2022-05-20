4004f1dde9659c69c9763746615cbf0e45590bc2
Georgiy Sapunov <GeorgiySapunov@yandex.ru>
2022-05-20 20:15:26 +0300

    lua neovim

--- .config/nvim/lua/user/vimtex.lua
+++ .config/nvim/lua/user/vimtex.lua
@@ -1,5 +1,4 @@
+vim.cmd([[
 let g:tex_flavor = 'latex' "Уточняем какой Тех
 let g:vimtex_view_method='zathura' " надо сначала установить
-
-"Отключаем автоматическое открытие окна Quickfix
-"let g:vimtex_quickfix_mode = 0
+]])
